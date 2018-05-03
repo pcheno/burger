939e4e6261c28241f1c1526264dd845802b7452a
Paul Cheno <pwchenoweth@gmail.com>
2018-05-02 20:41:29 -0500

    db connect stuff

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 var mysql = require("mysql");
-
+var connection;
 if (process.env.JAWSDB_URL) {
-  var conection = mysql.createConnection(process.env.JAWSDB_URL);
+   conection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    var connection = mysql.createConnection({
+     connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "root",
